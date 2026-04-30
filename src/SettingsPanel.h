91fc30a22230ae24021452c15c61ff24d134d57e
AlexsdeG <alex.remmarg@aim.com>
2026-05-01 00:57:15 +0200

    fix x close icon button in settings

--- src/SettingsPanel.h
+++ src/SettingsPanel.h
@@ -39,7 +39,7 @@ private:
 	juce::AudioDeviceSelectorComponent masterSelector;
 	juce::AudioDeviceSelectorComponent headphoneSelector;
 
-	juce::TextButton closeButton{ "\u00d7" };
+	juce::TextButton closeButton{ "X" };
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SettingsPanel)
 };
